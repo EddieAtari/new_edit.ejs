39dcf6046d4272773fca49d44afb61561454d425
Ed Westfield Jr <30934590+EddieAtari@users.noreply.github.com>
2021-01-02 11:22:03 -0600

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,19 @@
+// New 
+
+app.get('/farms/new',  (req, res) => {
+	const farm = null; 
+	res.render('farms/new_edit', { farm });
+});
+
+// Edit    GET    /farms/:id/edit  - form to update products
+
+app.get('/farms/:id/edit', wrapAsync(async (req, res, next) => {
+	const { id } = req.params;
+	const farm = await Farm.findById(id);
+	console.log(farm);
+	if (farm) {
+		res.render('farms/new_edit', { farm });
+	} else {
+		throw new AppError('farm Not Found!!!', 404);
+	}
+}));
